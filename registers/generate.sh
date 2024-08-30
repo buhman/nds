46cfc93060eff0148a170c68a3df06598203c5c6
Zack Buhman <zack@buhman.org>
2024-08-30 04:24:49 -0500

    remove DB_ prefix

--- registers/generate.sh
+++ registers/generate.sh
@@ -1,2 +1,2 @@
 python format.py 0x04000000.txt graphics_engine_a > graphics_engine_a.h
-python format.py 0x04001000.txt graphics_engine_a > graphics_engine_b.h
+python format.py 0x04001000.txt graphics_engine_b > graphics_engine_b.h
